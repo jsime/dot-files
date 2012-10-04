c8056f636ff10c90890009632248558664c06e83
Jon Sime <jonsime@gmail.com>
2012-10-04 16:28:20 -0400

    perltidyrc and .vim/ symlinks

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+pushd `dirname $0` >/dev/null
+SCRIPTDIR=`pwd -P`
+popd >/dev/null
+
 echo "Setting up vim..."
 rm -rf ~/.vimrc ~/.vim
-ln -s ./vimrc ~/.vimrc
-ln -s ./vim ~/.vim
+ln -s $SCRIPTDIR/vimrc ~/.vimrc
+ln -s $SCRIPTDIR/vim ~/.vim
 
 echo "Setting up screen..."
 rm ~/.screenrc
-ln -s ./screenrc ~/.screenrc
+ln -s $SCRIPTDIR/screenrc ~/.screenrc
+
+echo "Setting up perltidy..."
+rm ~/.perltidyrc
+ln -s $SCRIPTDIR/perltidyrc ~/.perltidyrc
 
 echo "Done!"
