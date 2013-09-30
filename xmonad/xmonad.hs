fedd3279c8e10ce18c0c3b1c3f1771b6a4bd7e13
Jon Sime <jonsime@gmail.com>
2013-09-30 11:30:42 -0400

    include mappings for volume keys in xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,19 @@
 import XMonad
+import qualified Data.Map as M
+import Graphics.X11.Xlib
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
 
 main = xmonad defaultConfig
     { modMask = mod4Mask
     , borderWidth = 0
     , terminal = "urxvt"
     }
+
+newKeys x = M.union (keys defaultConfig x) (mkKeymap x myKeys)
+myKeys = [
+  ("<XF86AudioLowerVolume>", spawn "amixer sset -- Master playback 10-"),
+  ("<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 10+"),
+  ("S-<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 100%"),
+  ("<XF86AudioMute>", spawn "amixer sset -- Master playback 0%")
+  ]
