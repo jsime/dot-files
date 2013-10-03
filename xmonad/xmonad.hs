adf802481f9a138fd5e92f47f5cbb54bff9980ad
Jon Sime <jonsime@gmail.com>
2013-10-03 11:03:02 -0400

    add keybinding in xmonad for screen locking

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,19 +1,24 @@
 import XMonad
 import qualified Data.Map as M
 import Graphics.X11.Xlib
-import XMonad.Util.EZConfig
+import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run
+import System.IO
 
-main = xmonad defaultConfig
-    { modMask = mod4Mask
-    , borderWidth = 0
-    , terminal = "urxvt"
-    }
+--main = xmonad defaultConfig
+main = do
+    xmonad $ defaultConfig
+        { modMask = mod4Mask
+        , borderWidth = 0
+        , terminal = "urxvt"
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        ]
 
 newKeys x = M.union (keys defaultConfig x) (mkKeymap x myKeys)
 myKeys = [
-  ("<XF86AudioLowerVolume>", spawn "amixer sset -- Master playback 10-"),
-  ("<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 10+"),
-  ("S-<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 100%"),
-  ("<XF86AudioMute>", spawn "amixer sset -- Master playback 0%")
-  ]
+        ("<XF86AudioLowerVolume>", spawn "amixer sset -- Master playback 10-"),
+        ("<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 10+"),
+        ("S-<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 100%"),
+        ("<XF86AudioMute>", spawn "amixer sset -- Master playback 0%")
+    ]
