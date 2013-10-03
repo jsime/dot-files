154362ee0024e1758541ffaba6f00feb1ff8dd4d
Jon Sime <jonsime@gmail.com>
2013-10-03 12:40:30 -0400

    fix volume control keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,14 @@
 import XMonad
 import qualified Data.Map as M
 import Graphics.X11.Xlib
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeys, mkKeymap)
 import XMonad.Util.Run
 import System.IO
+
+xF86AudioRaiseVolume,xF86AudioLowerVolume, xF86AudioMute :: KeySym
+xF86AudioRaiseVolume = 0x1008ff13
+xF86AudioLowerVolume = 0x1008ff11
+xF86AudioMute = 0x1008ff12
 
 main = do
     xmonad $ defaultConfig
@@ -12,12 +17,9 @@ main = do
         , terminal = "urxvt"
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((0, xF86AudioLowerVolume), spawn "amixer set Master playback 2.5dB- -q")
+        , ((0, xF86AudioRaiseVolume), spawn "amixer set Master playback 2.5dB+ -q")
+        , ((shiftMask, xF86AudioLowerVolume), spawn "amixer set Master playback 10dB- -q")
+        , ((shiftMask, xF86AudioRaiseVolume), spawn "amixer set Master playback 10dB+ -q")
+        , ((0, xF86AudioMute), spawn "amixer set Master playback 0% -q")
         ]
-
---newKeys x = M.union (keys defaultConfig x) (mkKeymap x myKeys)
---myKeys = [
---        ("<XF86AudioLowerVolume>", spawn "amixer sset -- Master playback 10-"),
---        ("<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 10+"),
---        ("S-<XF86AudioRaiseVolume>", spawn "amixer sset -- Master playback 100%"),
---        ("<XF86AudioMute>", spawn "amixer sset -- Master playback 0%")
---    ]
